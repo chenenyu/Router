8c9c93b19377b5dda8762273dfd47253c464c0ed
chenenyu <echocey@gmail.com>
2017-01-03 17:12:57 +0800

    Perfect the url matcher.

--- router/src/main/java/com/chenenyu/router/UrlMatcher.java
+++ router/src/main/java/com/chenenyu/router/UrlMatcher.java
@@ -1,15 +1,37 @@
 package com.chenenyu.router;
 
 import android.net.Uri;
-import android.text.TextUtils;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
+ * Standard url matcher. It matches scheme, authority(host, port) and path,
+ * then transfers the query part(if offered) to bundle.
+ * <p>
+ * If you configured a route like this:
+ * <code>
+ * <p>
+ * -> @Route("http://example.com/user?{id}&{status}")
+ * <p>
+ * </code>
+ * Then <a href="">http://example.com/user</a> will match this route,
+ * <a href="">http://example.com/user?id=9527&status=0</a> also does and with bundle:
+ * <code>
+ * <p>
+ * bundle.putString("id", "9527");
+ * <br>
+ * bundle.putString("status", "0");
+ * <p>
+ * </code>
  * <p>
  * Created by Cheney on 2016/12/30.
  */
 public class UrlMatcher implements Matcher {
     @Override
-    public boolean match(Uri uri, String path) {
+    public boolean match(Uri uri, String path, RouteOptions routeOptions) {
         if (uri.toString().equals(path)) {
             return true;
         }
@@ -19,18 +41,42 @@ public class UrlMatcher implements Matcher {
                 // http != https
                 return false;
             }
-            if (TextUtils.isEmpty(uri.getAuthority()) && TextUtils.isEmpty(route.getAuthority())) {
-                // host1 == host2 == 空
+            if (isEmpty(uri.getAuthority()) && isEmpty(route.getAuthority())) {
+                // host1 == host2 == empty
                 return true;
             }
-            if (!TextUtils.isEmpty(uri.getAuthority()) && !TextUtils.isEmpty(route.getAuthority())
+            // google.com == google.com (include port)
+            if (!isEmpty(uri.getAuthority()) && !isEmpty(route.getAuthority())
                     && uri.getAuthority().equals(route.getAuthority())) {
-                // google.com == google.com (include port)
                 if (!cutSlash(uri.getPath()).equals(cutSlash(route.getPath()))) {
-                    // /main != /home
                     return false;
                 }
-                // TODO: 2016/12/30 check query
+
+                // bundle parser
+                if (route.getQuery() != null && uri.getQuery() != null) {
+                    // parse entry from given uri.
+                    Map<String, String> params = new HashMap<>();
+                    parseParams(params, uri.getQuery());
+                    Log.d("UrlMatcher", "uri.getQuery(): " + uri.getQuery());
+                    Log.d("UrlMatcher", "params: " + params);
+
+                    String[] placeholders = route.getQuery().split("&");
+                    for (String placeholder : placeholders) {
+                        if (placeholder.startsWith("{") && placeholder.endsWith("}")) {
+                            placeholder = placeholder.substring(1, placeholder.length() - 1);
+                            Log.d("UrlMatcher", "placeholder: " + placeholder);
+                            if (params.containsKey(placeholder)) {
+                                Bundle bundle = routeOptions.getBundle();
+                                if (bundle == null) {
+                                    bundle = new Bundle();
+                                    routeOptions.setBundle(bundle);
+                                }
+                                bundle.putString(placeholder, params.get(placeholder));
+                            }
+                        }
+                    }
+                }
+                return true;
             }
         }
         return false;
@@ -50,5 +96,26 @@ public class UrlMatcher implements Matcher {
             return cutSlash(path.substring(0, path.length() - 1));
         }
         return path;
+    }
+
+    /**
+     * {@link android.text.TextUtils#isEmpty(CharSequence)}.
+     */
+    private boolean isEmpty(CharSequence str) {
+        return str == null || str.length() == 0;
+    }
+
+    private void parseParams(Map<String, String> map, String query) {
+        if (query != null && !query.isEmpty()) {
+            String[] entries = query.split("&");
+            for (String entry : entries) {
+                if (entry.contains("=")) {
+                    String[] kv = entry.split("=");
+                    if (kv.length > 1) {
+                        map.put(kv[0], kv[1]);
+                    }
+                }
+            }
+        }
     }
 }
