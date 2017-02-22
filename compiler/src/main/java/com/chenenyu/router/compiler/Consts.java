fee17f10d6979da9b9c8ba40c81ebd823ae7dabe
chenenyu <echocey@gmail.com>
2017-02-22 17:16:19 +0800

    v0.3.0

--- compiler/src/main/java/com/chenenyu/router/compiler/Consts.java
+++ compiler/src/main/java/com/chenenyu/router/compiler/Consts.java
@@ -5,8 +5,10 @@ package com.chenenyu.router.compiler;
  * Created by Cheney on 2017/1/12.
  */
 public class Consts {
-    public static final String INTERFACE_NAME = "com.chenenyu.router.RouteTable";
     public static final String PACKAGE_NAME = "com.chenenyu.router";
-    public static final String CLASS_SUFFIX = "RouteTable";
-    public static final String METHOD_NAME = "handleActivityTable";
+    public static final String ROUTE_TABLE_INTERFACE_NAME = "com.chenenyu.router.RouteTable";
+    public static final String ROUTE_TABLE_CLASS_SUFFIX = "RouteTable";
+    public static final String ROUTE_TABLE_METHOD_NAME = "handleActivityTable";
+    public static final String ROUTER_CONFIG_CLASS_NAME = "RouterBuildConfig";
+    public static final String ROUTER_CONFIG_FIELD_NAME = "ALL_MODULES";
 }
