8b61a4ca8d3fb1ce3ae21645c492f2d92f43f01f
chenenyu <echocey@gmail.com>
2017-03-02 10:19:55 +0800

    Remove generation work for RouterBuildConfig

--- compiler/src/main/java/com/chenenyu/router/compiler/Consts.java
+++ compiler/src/main/java/com/chenenyu/router/compiler/Consts.java
@@ -9,6 +9,4 @@ public class Consts {
     public static final String ROUTE_TABLE_INTERFACE_NAME = "com.chenenyu.router.RouteTable";
     public static final String ROUTE_TABLE_CLASS_SUFFIX = "RouteTable";
     public static final String ROUTE_TABLE_METHOD_NAME = "handleActivityTable";
-    public static final String ROUTER_CONFIG_CLASS_NAME = "RouterBuildConfig";
-    public static final String ROUTER_CONFIG_FIELD_NAME = "ALL_MODULES";
 }
