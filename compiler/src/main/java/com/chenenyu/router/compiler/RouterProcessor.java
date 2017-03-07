d9363751b7fd7e3b3b6e05e548e46d88d8b46c53
chenenyu <echocey@gmail.com>
2017-03-07 18:16:08 +0800

    Optimize RouterProcessor

--- compiler/src/main/java/com/chenenyu/router/compiler/RouterProcessor.java
+++ compiler/src/main/java/com/chenenyu/router/compiler/RouterProcessor.java
@@ -24,23 +24,28 @@ import javax.annotation.processing.SupportedOptions;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
 
+import static com.chenenyu.router.compiler.Consts.ACTIVITY_FULL_NAME;
+import static com.chenenyu.router.compiler.Consts.OPTION_MODULE_NAME;
+import static com.chenenyu.router.compiler.Consts.PACKAGE_NAME;
+import static com.chenenyu.router.compiler.Consts.ROUTE_ANNOTATION_TYPE;
+import static com.chenenyu.router.compiler.Consts.ROUTE_TABLE;
+import static com.chenenyu.router.compiler.Consts.ROUTE_TABLE_FULL_NAME;
+import static com.chenenyu.router.compiler.Consts.ROUTE_TABLE_METHOD_NAME;
+
 /**
- * Route annotation processor.
+ * {@link Route} annotation processor.
  * <p>
  * Created by Cheney on 2016/12/20.
  */
-@SupportedAnnotationTypes("com.chenenyu.router.annotation.Route")
-@SupportedOptions({"moduleName"})
+@SupportedAnnotationTypes(ROUTE_ANNOTATION_TYPE)
+@SupportedOptions(OPTION_MODULE_NAME)
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class RouterProcessor extends AbstractProcessor {
-    private static final String OPTION_MODULE_NAME = "moduleName";
-
     private Elements elementUtils = null;
     private Filer filer = null;
     private Messager messager = null;
@@ -65,48 +70,42 @@ public class RouterProcessor extends AbstractProcessor {
         // 合法的TypeElement集合
         Set<TypeElement> typeElements = new HashSet<>();
         for (Element element : elements) {
-            try {
-                validateElement(element);
+            if (validateElement(element)) {
                 typeElements.add((TypeElement) element);
-            } catch (RouteException e) {
-                error(e.getElement(), e.getMessage());
-                return true;
             }
         }
         String moduleName = processingEnv.getOptions().get(OPTION_MODULE_NAME);
         if (moduleName != null) {
-            generateCode(combineClzName(moduleName), typeElements);
+            generateCode(moduleName, typeElements);
         } else {
-            error(null, "No option %s passed to annotation processor.", OPTION_MODULE_NAME);
+            error(null, "No option `%s` passed to annotation processor.", OPTION_MODULE_NAME);
         }
         return true;
     }
 
     /**
      * Verify the annotated class.
-     *
-     * @param typeElement TypeElement
-     * @throws RouteException Illegal annotation.
      */
-    private void validateElement(Element typeElement) throws RouteException {
-        // element annotated by @Route instance of CLASS
-        if (typeElement.getKind() != ElementKind.CLASS) {
-            throw new RouteException(String.format("The annotated element is not a class, but a %s.",
-                    typeElement.getKind().name()), typeElement);
+    private boolean validateElement(Element typeElement) {
+        if (!processingEnv.getTypeUtils().isSubtype(typeElement.asType(),
+                processingEnv.getElementUtils().getTypeElement(ACTIVITY_FULL_NAME).asType())) {
+            error(typeElement, "%s is not a subclass of `%s`.",
+                    typeElement.getSimpleName().toString(), ACTIVITY_FULL_NAME);
+            return false;
         }
         Set<Modifier> modifiers = typeElement.getModifiers();
-        // non-public class, error.
+        // non-public class.
         if (!modifiers.contains(Modifier.PUBLIC)) {
-            throw new RouteException(String.format("The class %s is not be public.",
-                    ((TypeElement) typeElement).getQualifiedName()), typeElement);
+            error(typeElement, "The class %s is not public.", ((TypeElement) typeElement).getQualifiedName());
+            return false;
         }
-        // abstract class, skip.
+        // abstract class.
         if (modifiers.contains(Modifier.ABSTRACT)) {
-            throw new RouteException(String.format(
-                    "The class %s is abstract. You can't annotate abstract classes with @%s.",
-                    ((TypeElement) typeElement).getQualifiedName(), Route.class.getSimpleName()),
-                    typeElement);
+            error(typeElement, "The class %s is abstract. You can't annotate abstract classes with @%s.",
+                    ((TypeElement) typeElement).getQualifiedName(), Route.class.getSimpleName());
+            return false;
         }
+        return true;
     }
 
     private void error(Element element, String message, Object... args) {
@@ -116,15 +115,15 @@ public class RouterProcessor extends AbstractProcessor {
     /**
      * Generate a .java file that contains route map.
      */
-    private void generateCode(String clzName, Set<TypeElement> elements) {
-        TypeElement activityType = elementUtils.getTypeElement("android.app.Activity");
+    private void generateCode(String moduleName, Set<TypeElement> elements) {
+        TypeElement activityType = elementUtils.getTypeElement(ACTIVITY_FULL_NAME);
         // Map<String, Class<? extends Activity>> map
         ParameterizedTypeName mapTypeName = ParameterizedTypeName.get(ClassName.get(Map.class),
                 ClassName.get(String.class), ParameterizedTypeName.get(ClassName.get(Class.class),
                         WildcardTypeName.subtypeOf(ClassName.get(activityType))));
         ParameterSpec mapParameterSpec = ParameterSpec.builder(mapTypeName, "map").build();
 
-        MethodSpec.Builder initActivityTable = MethodSpec.methodBuilder(Consts.ROUTE_TABLE_METHOD_NAME)
+        MethodSpec.Builder handleActivityTable = MethodSpec.methodBuilder(ROUTE_TABLE_METHOD_NAME)
                 .addAnnotation(Override.class)
                 .addModifiers(Modifier.PUBLIC)
                 .addParameter(mapParameterSpec);
@@ -132,26 +131,22 @@ public class RouterProcessor extends AbstractProcessor {
             Route route = element.getAnnotation(Route.class);
             String[] paths = route.value();
             for (String path : paths) {
-                initActivityTable.addStatement("map.put($S, $T.class)", path, ClassName.get(element));
+                handleActivityTable.addStatement("map.put($S, $T.class)", path, ClassName.get(element));
             }
         }
 
-        TypeElement interfaceType = elementUtils.getTypeElement(Consts.ROUTE_TABLE_INTERFACE_NAME);
-        TypeSpec type = TypeSpec.classBuilder(clzName)
+        TypeElement interfaceType = elementUtils.getTypeElement(ROUTE_TABLE_FULL_NAME);
+        TypeSpec type = TypeSpec.classBuilder(capitalize(moduleName) + ROUTE_TABLE)
                 .addSuperinterface(ClassName.get(interfaceType))
                 .addModifiers(Modifier.PUBLIC)
-                .addMethod(initActivityTable.build())
+                .addMethod(handleActivityTable.build())
                 .addJavadoc("Generated by Router. Do not edit it!\n")
                 .build();
         try {
-            JavaFile.builder(Consts.PACKAGE_NAME, type).build().writeTo(filer);
+            JavaFile.builder(PACKAGE_NAME, type).build().writeTo(filer);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private String combineClzName(String moduleName) {
-        return capitalize(moduleName) + Consts.ROUTE_TABLE_CLASS_SUFFIX;
     }
 
     private String capitalize(CharSequence self) {
