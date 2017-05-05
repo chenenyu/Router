f2951d8dc7cc635c3d99a0e7a422a5d9f3d1b26a
chenenyu <echocey@gmail.com>
2017-05-05 18:12:43 +0800

    Optimize.

--- router/src/main/java/com/chenenyu/router/AptHub.java
+++ router/src/main/java/com/chenenyu/router/AptHub.java
@@ -20,9 +20,9 @@ class AptHub {
     private static final String BUILD_INFO_FIELD = "ALL_MODULES";
     private static final String ROUTE_TABLE = "RouteTable";
     private static final String INTERCEPTORS = "Interceptors";
-    private static final String HANDLE_INTERCEPTORS = "handleInterceptors";
+    private static final String HANDLE_INTERCEPTORS = "handle";
     private static final String INTERCEPTOR_TABLE = "InterceptorTable";
-    private static final String HANDLE_INTERCEPTOR_TABLE = "handleInterceptorTable";
+    private static final String HANDLE_INTERCEPTOR_TABLE = "handle";
 
     // Uri -> Activity/Fragment
     static Map<String, Class<?>> routeTable = new HashMap<>();
@@ -35,8 +35,7 @@ class AptHub {
         /* RouterBuildInfo */
         String[] modules;
         try {
-            Class<?> buildInfo = Class.forName(PACKAGE_NAME + DOT +
-                    ROUTER_BUILD_INFO);
+            Class<?> buildInfo = Class.forName(PACKAGE_NAME + DOT + ROUTER_BUILD_INFO);
             Field allModules = buildInfo.getField(BUILD_INFO_FIELD);
             String modules_name = (String) allModules.get(buildInfo);
             modules = modules_name.split(",");
@@ -53,14 +52,15 @@ class AptHub {
         String fullTableName;
         for (String moduleName : modules) {
             try {
-                fullTableName = PACKAGE_NAME + DOT + capitalize(moduleName) +
-                        ROUTE_TABLE;
+                fullTableName = PACKAGE_NAME + DOT + capitalize(moduleName) + ROUTE_TABLE;
                 Class<?> routeTableClz = Class.forName(fullTableName);
                 Constructor constructor = routeTableClz.getConstructor();
                 RouteTable instance = (RouteTable) constructor.newInstance();
                 instance.handle(routeTable);
+            } catch (ClassNotFoundException e) {
+                RLog.i(String.format("There is no route table in module: %s.", moduleName));
             } catch (Exception e) {
-                RLog.i(e.getMessage());
+                RLog.w(e.getMessage());
             }
         }
         RLog.i("RouteTable", routeTable.toString());
@@ -69,33 +69,37 @@ class AptHub {
         String interceptorTableName;
         for (String moduleName : modules) {
             try {
-                interceptorTableName = PACKAGE_NAME + DOT + capitalize(moduleName) +
-                        INTERCEPTOR_TABLE;
-                Class<?> interceptorTableClz = Class.forName(interceptorTableName);
-                Method handleInterceptorTable = interceptorTableClz.getMethod(
-                        HANDLE_INTERCEPTOR_TABLE, Map.class);
-                handleInterceptorTable.invoke(null, interceptorTable);
-            } catch (Exception e) {
+                interceptorTableName = PACKAGE_NAME + DOT + capitalize(moduleName) + INTERCEPTOR_TABLE;
+                Class<?> clz = Class.forName(interceptorTableName);
+                Method handle = clz.getMethod(HANDLE_INTERCEPTOR_TABLE, Map.class);
+                handle.invoke(null, interceptorTable);
+            } catch (ClassNotFoundException e) {
                 RLog.i(String.format("There is no interceptor table in module: %s.", moduleName));
+            } catch (Exception e) {
+                RLog.w(e.getMessage());
             }
         }
-        RLog.i("InterceptorTable", interceptorTable.toString());
+        if (!interceptorTable.isEmpty()) {
+            RLog.i("InterceptorTable", interceptorTable.toString());
+        }
 
         /* Interceptors */
         String interceptorName;
         for (String moduleName : modules) {
             try {
-                interceptorName = PACKAGE_NAME + DOT + capitalize(moduleName) +
-                        INTERCEPTORS;
+                interceptorName = PACKAGE_NAME + DOT + capitalize(moduleName) + INTERCEPTORS;
                 Class<?> interceptorClz = Class.forName(interceptorName);
-                Method handleInterceptors = interceptorClz.getMethod(HANDLE_INTERCEPTORS,
-                        Map.class);
-                handleInterceptors.invoke(null, interceptors);
-            } catch (Exception e) {
+                Method handle = interceptorClz.getMethod(HANDLE_INTERCEPTORS, Map.class);
+                handle.invoke(null, interceptors);
+            } catch (ClassNotFoundException e) {
                 RLog.i(String.format("There are no interceptors in module: %s.", moduleName));
+            } catch (Exception e) {
+                RLog.w(e.getMessage());
             }
         }
-        RLog.i("Interceptors", interceptors.toString());
+        if (!interceptors.isEmpty()) {
+            RLog.i("Interceptors", interceptors.toString());
+        }
     }
 
     private static String capitalize(CharSequence self) {
