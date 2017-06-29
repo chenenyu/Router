8f2801335a33b03246a69bd0b7f9a3574250c084
chenenyu <echocey@gmail.com>
2017-06-29 11:28:18 +0800

    Add new test case in demo.

---
+++ Sample/app/src/main/java/com/chenenyu/router/app/Model.java
@@ -0,0 +1,12 @@
+package com.chenenyu.router.app;
+
+import java.io.Serializable;
+
+/**
+ * Test model.
+ * <p>
+ * Created by Enyu Chen on 2017/6/28.
+ */
+public class Model implements Serializable {
+    // empty
+}
