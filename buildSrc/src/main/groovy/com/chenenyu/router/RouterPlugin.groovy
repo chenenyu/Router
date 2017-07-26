69d0d9821e37193ff426924f709898b24dd96ad2
chenenyu <echocey@gmail.com>
2017-07-26 10:49:06 +0800

    1. Del Reference to Android Gradle Plugin. 2. Simplify the way to pass apt options.

--- buildSrc/src/main/groovy/com/chenenyu/router/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/com/chenenyu/router/RouterPlugin.groovy
@@ -1,12 +1,5 @@
 package com.chenenyu.router
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.LibraryPlugin
-import com.android.build.gradle.internal.api.ApplicationVariantImpl
-import com.android.build.gradle.internal.api.LibraryVariantImpl
-import com.android.utils.FileUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -17,22 +10,29 @@ import org.gradle.api.plugins.ExtraPropertiesExtension
  * Created by Cheney on 2017/1/10.
  */
 class RouterPlugin implements Plugin<Project> {
+    static final String APP = "com.android.application"
+    static final String LIBRARY = "com.android.library"
     static final String APT_OPTION_NAME = "moduleName"
 
     @Override
     void apply(Project project) {
-        if (!(project.plugins.hasPlugin(AppPlugin) || project.plugins.hasPlugin(LibraryPlugin))) {
+        if (!(project.plugins.hasPlugin(APP) || project.plugins.hasPlugin(LIBRARY))) {
             throw new IllegalArgumentException(
                     'Router gradle plugin can only be applied to android projects.')
         }
-
-        // project.logger.error("----- Router begin: ${project.name}-----")
 
         def isKotlinProject = project.plugins.hasPlugin('kotlin-android')
         if (isKotlinProject) {
             if (!project.plugins.hasPlugin('kotlin-kapt')) {
                 project.plugins.apply('kotlin-kapt')
             }
+        }
+
+        // Add annotationProcessorOptions
+        def android = project.extensions.android // BaseExtension
+        android.defaultConfig.javaCompileOptions.annotationProcessorOptions.argument(APT_OPTION_NAME, project.name)
+        android.productFlavors.all {
+            it.javaCompileOptions.annotationProcessorOptions.argument(APT_OPTION_NAME, project.name)
         }
 
         // Add dependencies
@@ -73,55 +73,43 @@ class RouterPlugin implements Plugin<Project> {
 
         project.afterEvaluate {
             project.rootProject.subprojects.each {
-                if (it.plugins.hasPlugin(AppPlugin) && !it.plugins.hasPlugin(RouterPlugin)) {
-                    project.logger.error("Have you forgotten to apply plugin 'com.chenenyu.router' " +
-                            "in module: '${it.name}'?")
+                if (it.plugins.hasPlugin(APP) && !it.plugins.hasPlugin(RouterPlugin)) {
+                    project.logger.error("Have you forgotten to apply plugin: 'com.chenenyu.router'" +
+                            " in module: '${it.name}'?")
                 }
             }
 
-            if (project.plugins.hasPlugin(AppPlugin)) {
+            if (project.plugins.hasPlugin(APP)) {
                 // Read template in advance, it can't be read in GenerateBuildInfoTask.
                 InputStream is = RouterPlugin.class.getResourceAsStream("/RouterBuildInfo.template")
                 String template
                 new Scanner(is).with {
                     template = it.useDelimiter("\\A").next()
                 }
-                File routerFolder = FileUtils.join(project.buildDir, "generated", "source", "router")
+                File routerFolder = new File(project.buildDir,
+                        "generated" + File.separator + "source" + File.separator + "router")
 
                 // Record lib modules' name
                 StringBuilder sb = new StringBuilder()
                 Set<Project> subs = project.rootProject.subprojects
                 subs.each {
-                    if (it.plugins.hasPlugin(LibraryPlugin) && it.plugins.hasPlugin(RouterPlugin)) {
+                    if (it.plugins.hasPlugin(LIBRARY) && it.plugins.hasPlugin(RouterPlugin)) {
                         sb.append(it.name.replace('.', '_').replace('-', '_')).append(",")
                     }
                 }
                 String validAppName = project.name.replace('.', '_').replace('-', '_')
                 sb.append(validAppName)
 
-                ((AppExtension) project.android).applicationVariants.all { ApplicationVariantImpl variant ->
+                project.android.applicationVariants.all { variant ->
                     // Create task
                     Task generateTask = project.tasks.create("generate${variant.name.capitalize()}BuildInfo", GenerateBuildInfoTask) {
-                        it.applicationVariant = variant
                         it.routerFolder = routerFolder
                         it.buildInfoContent = template.replaceAll("%ALL_MODULES%", sb.toString())
                     }
 
-                    variant.javaCompile.dependsOn generateTask
                     // Add generated file to javac source
                     variant.javaCompile.source(routerFolder)
-
-                    if (!isKotlinProject) {
-                        // Inspired by com.android.build.gradle.tasks.factory.JavaCompileConfigAction
-                        variant.javaCompile.options.compilerArgs.add("-A${APT_OPTION_NAME}=${project.name}")
-                    }
-                }
-            } else {
-                ((LibraryExtension) project.android).libraryVariants.all { LibraryVariantImpl variant ->
-                    if (!isKotlinProject) {
-                        // Inspired by com.android.build.gradle.tasks.factory.JavaCompileConfigAction
-                        variant.javaCompile.options.compilerArgs.add("-A${APT_OPTION_NAME}=${project.name}")
-                    }
+                    variant.javaCompile.dependsOn generateTask
                 }
             }
         }
