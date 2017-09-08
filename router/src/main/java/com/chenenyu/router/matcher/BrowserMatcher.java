03869f31eb43a5c61dd12c2b8fb62988d7243925
chenenyu <echocey@gmail.com>
2017-09-08 15:00:05 +0800

    Fix implicit bug: User calls a browser intent and the route table contains no mapping.

--- router/src/main/java/com/chenenyu/router/matcher/BrowserMatcher.java
+++ router/src/main/java/com/chenenyu/router/matcher/BrowserMatcher.java
@@ -19,7 +19,7 @@ public class BrowserMatcher extends AbsImplicitMatcher {
 
     @Override
     public boolean match(Context context, Uri uri, @Nullable String route, RouteRequest routeRequest) {
-        return !isEmpty(route) && (uri.toString().toLowerCase().startsWith("http://")
+        return (uri.toString().toLowerCase().startsWith("http://")
                 || uri.toString().toLowerCase().startsWith("https://"));
     }
 }
