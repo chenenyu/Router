1f6d4198f11e5116420e92e329d5720ca5cd351b
chenenyu <chenenyu@didichuxing.com>
2018-02-06 15:25:49 +0800

    1.4.0-beta1

---
+++ release.sh
@@ -0,0 +1,11 @@
+# annotation
+./gradlew :annotation:clean :annotation:pBPTML
+./gradlew :annotation:bintrayUpload
+
+# compiler
+./gradlew :compiler:clean :compiler:pBPTML
+./gradlew :compiler:bintrayUpload
+
+# router
+./gradlew :router:clean :router:install
+./gradlew :router:bintrayUpload
