e3a65b88bd8dd9a7e7f1b9af68d1d759cdb7f438
chenenyu <ceychina@gmail.com>
2018-08-02 15:48:52 +0800

    apply plugin

--- release.sh
+++ release.sh
@@ -1,11 +1,15 @@
 # annotation
-./gradlew clean pBPTML -p annotation
+./gradlew clean publish -p annotation
 ./gradlew bintrayUpload -p annotation
 
 # compiler
-./gradlew clean pBPTML -p compiler
+./gradlew clean publish -p compiler
 ./gradlew bintrayUpload -p compiler
 
 # router
-./gradlew clean pBPTML -p router
+./gradlew clean publish -p router
 ./gradlew bintrayUpload -p router
+
+# gradle-plugin
+./gradlew clean publish -p gradle-plugin
+./gradlew bintrayUpload -p gradle-plugin
