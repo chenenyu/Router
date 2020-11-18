d6123b2961725df25a8b845b96e2428bd1f7d6a3
chenenyu <chenenyux@gmail.com>
2020-11-18 15:29:58 +0800

    Update gradle

--- release.sh
+++ release.sh
@@ -1,15 +1,15 @@
 # annotation
-./gradlew clean pBPTML -p annotation
+./gradlew clean pRPTML -p annotation
 ./gradlew bintrayUpload -p annotation
 
 # compiler
-./gradlew clean pBPTML -p compiler
+./gradlew clean pRPTML -p compiler
 ./gradlew bintrayUpload -p compiler
 
 # router
-./gradlew clean pBPTML -p router
+./gradlew clean pRPTML -p router
 ./gradlew bintrayUpload -p router
 
 # gradle-plugin
-./gradlew clean pBPTML -p gradle-plugin
+./gradlew clean pRPTML -p gradle-plugin
 ./gradlew bintrayUpload -p gradle-plugin
